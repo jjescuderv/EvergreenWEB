1b09004f0f7182e9b7d85eef35d36a882100a29e
jjescuderv <47001432+jjescuderv@users.noreply.github.com>
2019-10-22 21:45:31 -0500

    Primer commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template, jsonify
+import requests
+
+app = Flask(__name__)
+
+tiposIndicador = ['Estrés hídrico', 'Nitrógeno foliar', 'Índice cosecha', 'Densidad volumétrica radial']
+
+@app.route("/", methods=['GET'])
+def crearIndicador():
+    return render_template('formularioIndicadores.html', listaTipos = tiposIndicador)
+
+@app.route("/listarIndicadores", methods=['GET'])
+def listarIndicadores():
+    indicadores_lista = requests.get('http://localhost:5000/indicadores').json()
+    return render_template('listaIndicadores.html', listaIndicadores = indicadores_lista)
+
+@app.route("/guardarIndicador", methods=['POST'])
+def guardarIndicador():
+    indicador = dict(request.values)
+    indicador['prioridad'] = int(indicador['prioridad'])
+    indicador['codigo'] = int(indicador['codigo'])
+    requests.post('http://localhost:5000/indicadores', json=indicador)
+    return crearIndicador()
+
+app.run(port=8000, debug=True)
